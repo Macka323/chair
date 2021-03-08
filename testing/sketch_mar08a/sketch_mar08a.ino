a99ece6223dbd85efe59333df364e11731ff2058
Macka323 <75259519+Macka323@users.noreply.github.com>
2021-03-08 12:30:44 +0100

    test

--- testing/sketch_mar08a/sketch_mar08a.ino
+++ testing/sketch_mar08a/sketch_mar08a.ino
@@ -1,13 +1,26 @@
-int x=0,r=0,g=0,b=0;
-int y;
-void setup(){
+int x[4];
+int serialdata = 0;
+void setup() {
   Serial.begin(9600);
 }
-void loop(){
-    if (Serial.available() > 0) {
-      y=Serial.read();      
-      Serial.flush();
-      y=2632554;
-      Serial.print(y);   
+void loop() {
+  serialdata = Serial.read();
+
+  if (Serial.available() > 0) {
+    if (serialdata >= 0 && serialdata <= 255) {
+      x[0] = serialdata;
     }
+    if (serialdata > 255 && serialdata <= 510) {
+      x[1] = serialdata;
+      x[1]=x[1]-255;
+    }
+    if (serialdata > 510 && serialdata <= 765) {
+      x[2] = serialdata;
+      x[2]=x[2]-510;
+    }
+    if (serialdata > 765 && serialdata <= 1020) {
+      x[3] = serialdata;
+      x[3]=x[3]-765;
+    }
+  }
 }
