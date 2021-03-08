f8e099f34aca3152a28aa6a2d6eec4e7c0c4d881
Macka323 <75259519+Macka323@users.noreply.github.com>
2021-03-08 15:10:55 +0100

    updating the esp32 code

--- testing/serial.ino
+++ testing/serial.ino
@@ -1,41 +1,38 @@
-/*int x;
-
-Serial.begin(9600);
-
-if (Serial.available() > 0){
-  x=Serial.read();
-  y=Serial.read();
-  r=Serial.read();
-  g=Serial.read();
-  b=Serial.read();
-
-  
-  
+//for support https://www.youtube.com/watch?v=q416XXJDE_M
+int x[4];
+int serialdata = 0;
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(10);
 }
-*/
+void loop() {
+  serialdata = Serial.parseInt();
 
-/*#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    long long x,y,r,g,b;
-    cin>>x;
-    y=x;
-    r=x;
-    g=x;
-    b=x;
-    
-    x=y/1000000000;
-    
-    r=y/1000000;
-    r=r%1000;
-    
-    g=y/1000;
-    g=g%1000000;
-    
-    b=y%1000000000;
-    
-    cout<<y<<endl<<x<<endl<<r<<endl<<g<<endl<<b<<endl;
-}*/
+ 
+    if (serialdata >= 1 && serialdata <= 256) {
+      x[0] = serialdata;
+      x[0] -= 1;
+    }
+    if (serialdata >= 257 && serialdata <= 512) {
+      x[1] = serialdata;
+      x[1]=x[1]-257;
+    }
+    if (serialdata >= 513 && serialdata <= 768) {
+      x[2] = serialdata;
+      x[2]=x[2]-513;
+    }
+    if (serialdata >= 769 && serialdata <= 1024) {
+      x[3] = serialdata;
+      x[3]=x[3]-769;
+    }
+    Serial.println("-----------");
+    Serial.print(x[0]);
+    Serial.println("x");
+    Serial.print(x[1]);
+    Serial.println("r");
+    Serial.print(x[2]);
+    Serial.println("g");
+    Serial.print(x[3]);
+    Serial.println("b");
+  delay(1000);
+}
--- testing/sketch_mar08a/sketch_mar08a.ino
+++ testing/sketch_mar08a/sketch_mar08a.ino
@@ -1,28 +1,31 @@
+//for support https://www.youtube.com/watch?v=q416XXJDE_M
 int x[4];
 int serialdata = 0;
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(10);
 }
 void loop() {
-  serialdata = Serial.read();
+  serialdata = Serial.parseInt();
 
  
     if (serialdata >= 1 && serialdata <= 256) {
       x[0] = serialdata;
-      x[0]=x[0]-1;
+      x[0] -= 1;
     }
-    if (serialdata >= 257 && serialdata <= 513) {
+    if (serialdata >= 257 && serialdata <= 512) {
       x[1] = serialdata;
-      x[1]=x[1]-256;
+      x[1]=x[1]-257;
     }
-    if (serialdata >= 514 && serialdata <= 770) {
+    if (serialdata >= 513 && serialdata <= 768) {
       x[2] = serialdata;
       x[2]=x[2]-513;
     }
-    if (serialdata > 771 && serialdata <= 1027) {
+    if (serialdata >= 769 && serialdata <= 1024) {
       x[3] = serialdata;
-      x[3]=x[3]-770;
+      x[3]=x[3]-769;
     }
+    Serial.println("-----------");
     Serial.print(x[0]);
     Serial.println("x");
     Serial.print(x[1]);
@@ -31,5 +34,5 @@ void loop() {
     Serial.println("g");
     Serial.print(x[3]);
     Serial.println("b");
-  
+  delay(1000);
 }
