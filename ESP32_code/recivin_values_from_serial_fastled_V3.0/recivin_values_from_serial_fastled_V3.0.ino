287d6f053bd5fbd5977f312751fe89e3030ba491
Macka323 <75259519+Macka323@users.noreply.github.com>
2021-03-11 10:28:02 +0100

    ESP32 code V3.0

--- ESP32_code/recivin_values_from_serial_fastled_V3.0/recivin_values_from_serial_fastled_V3.0.ino
+++ ESP32_code/recivin_values_from_serial_fastled_V3.0/recivin_values_from_serial_fastled_V3.0.ino
@@ -1,29 +1,37 @@
-//for support https://www.youtube.com/watch?v=q416XXJDE_M
+#include <FastLED.h>
+#define NUM_LEDS 1
+#define DATA_PIN 3
+
+CRGB leds[NUM_LEDS];
+int serialdata;
 int x[4];
-int serialdata = 0;
+
 void setup() {
   Serial.begin(38400);
-  Serial.setTimeout(10);
+  Serial.setTimeout(5);
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
 }
-void loop() {
-  serialdata = Serial.parseInt();
 
- 
+void loop() {
+  if (Serial.available() > 0) {
+    serialdata = Serial.parseInt();
+    
+
     if (serialdata >= 1 && serialdata <= 256) {
       x[0] = serialdata;
       x[0] -= 1;
     }
     if (serialdata >= 257 && serialdata <= 512) {
       x[1] = serialdata;
-      x[1]=x[1]-257;
+      x[1] = x[1] - 257;
     }
     if (serialdata >= 513 && serialdata <= 768) {
       x[2] = serialdata;
-      x[2]=x[2]-513;
+      x[2] = x[2] - 513;
     }
     if (serialdata >= 769 && serialdata <= 1024) {
       x[3] = serialdata;
-      x[3]=x[3]-769;
+      x[3] = x[3] - 769;
     }
     Serial.println("-----------");
     Serial.print(x[0]);
@@ -34,5 +42,6 @@ void loop() {
     Serial.println("g");
     Serial.print(x[3]);
     Serial.println("b");
-  delay(1000);
+     
+  }
 }
