2dd5d97b66b937f1cd3b8a3f7c638650354cbc6e
Martin Milchhevski <martin.milchevski2@gmail.com>
2022-05-16 21:26:15 +0200

    *

---
+++ website/Luma/Luma/Pages/Website.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Luma.Pages
+{
+    public class WebsiteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
