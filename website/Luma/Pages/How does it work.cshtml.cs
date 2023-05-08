f608a042d20d7b32365f6d8ba0c3fff7b6ecda81
Macka323 <martin.milchevski2@gmail.com>
2023-05-09 01:52:42 +0200

    *

---
+++ website/Luma/Pages/How does it work.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Luma.Pages
+{
+    public class How_does_it_workModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
